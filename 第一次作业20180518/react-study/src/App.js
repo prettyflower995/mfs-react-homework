35771814e915ee979e115a8a98aa23dd44b55bd9
prettyflower <995887551@qq.com>
2018-05-18 23:11:59 +0800

    second commit

---
+++ 第一次作业20180518/react-study/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class HelloComponent  extends Component {
+  render(){
+    return (
+      <div>hello world</div>
+    )
+  }
+}
+
+export default HelloComponent;
